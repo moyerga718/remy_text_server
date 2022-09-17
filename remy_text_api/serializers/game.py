05fb85abe9f12994895d7a57e1d6b9e315b7e50b
Griffin Moyer <moyerga@outlook.com>
2022-09-17 09:58:05 -0500

    final story fixtures and handle ending

--- remy_text_api/serializers/game.py
+++ remy_text_api/serializers/game.py
@@ -4,5 +4,5 @@ from remy_text_api.models import Game
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ('id','first_name','current_situation','items')
+        fields = ('id','first_name','current_situation','items', 'completed')
         depth = 1
