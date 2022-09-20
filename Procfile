4aad70e2184e619102e9426a47f226ef3b2a31bd
Griffin Moyer <moyerga@outlook.com>
2022-09-20 15:49:01 -0500

    heroku deployment files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn remy-text-server.wsgi --log-file -
--- remy_text/settings.py
+++ remy_text/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,3 +141,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
