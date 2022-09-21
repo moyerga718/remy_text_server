2be45f152b6daa4dcd0cb678d5484c22a949cfda
Griffin Moyer <moyerga@outlook.com>
2022-09-21 13:45:34 -0500

    added heroku url to settings.py

--- remy_text/settings.py
+++ remy_text/settings.py
@@ -55,7 +55,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://remy-adventure.herokuapp.com/'
 )
 
 MIDDLEWARE = [
